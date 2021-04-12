7525cbc8e990e546b0d05456ea528f2789d4ef69
Hanna-Kushnarenko <65454544+Hanna-Kushnarenko@users.noreply.github.com>
2021-04-12 15:42:51 +0300

    Update scenario.sh

--- scenario.sh
+++ scenario.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+
 ml icc
 echo "g++ compilation with 0 flag:"
-g++ -O Circle.cpp -o test0
+g++ -O circle.cpp -o test0
 time ./test0
 echo -e "\n"
 
 echo "g++ compilation with 01 flag:"
-g++ -O1 Circle.cpp -o test1
+g++ -O1 circle.cpp -o test1
 time ./test1
 echo -e "\n"
 
 echo "g++ compilation with 02 flag:"
-g++ -O2 Circle.cpp -o test2
+g++ -O2 circle.cpp -o test2
 time ./test2
 echo -e "\n"
 
 echo "g++ compilation with 03 flag:"
-g++ -O3 Circle.cpp -o test3
+g++ -O3 circle.cpp -o test3
 time ./test3
 echo -e "\n"
 
 echo "g++ compilation with 0fast flag:"
-g++ -Ofast Circle.cpp -o test4
+g++ -Ofast circle.cpp -o test4
 time ./test4
 echo -e "\n"
