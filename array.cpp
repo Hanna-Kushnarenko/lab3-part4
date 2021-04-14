2ecf14d24563278a34f1f063f11302fb9927e16d
Hanna-Kushnarenko <65454544+Hanna-Kushnarenko@users.noreply.github.com>
2021-04-14 17:31:26 +0300

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -2,16 +2,15 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-void rec(long long a, int b, int t, int n)
+void rec(int a, int b, int t, long long n)
 {
-    a=10e16;
+    //cout<<a<<endl;
     if(t<n)
         rec(b, a+b, t+1, n);
 }
 int main()
 {
-    int n;
-    cin>>n;
+    long long n= 10e16;
     rec(2,1,1,n);
     return 0;
 }
