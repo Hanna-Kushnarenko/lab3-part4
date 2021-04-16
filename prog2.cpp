3c5f72539f753f013b7bf240cb9f5f0e05831690
Hanna-Kushnarenko <65454544+Hanna-Kushnarenko@users.noreply.github.com>
2021-04-16 19:19:54 +0300

    Update prog2.cpp

--- prog2.cpp
+++ prog2.cpp
@@ -5,23 +5,66 @@
  
 using namespace std;
  
-int main(int argc, char **argv)
+#define MAX_NUM 50
+// array will be initialized to 0 being global
+int primes[1000];
+ 
+void gen_sieve_primes(void)
 {
-    int p = 7, l = 3, g = 2, n = 4, x;
-    int a[] = { 1, 2, 2, 1 };
-    int bin[4];
-    cout << "The Random numbers are: ";
-    for (int i = 0; i < 10; i++)
+    for (int p = 2; p < MAX_NUM; p++) // for all elements in array
     {
-        x = rand() % 16;
-        for (int j = 3; j >= 0; j--)
+        if (primes[p] == 0) // it is not multiple of any other prime
+            primes[p] = 1; // mark it as prime
+ 
+        // mark all multiples of prime selected above as non primes
+        int c = 2;
+        int mul = p * c;
+        for (; mul < MAX_NUM;)
         {
-            bin[j] = x % 2;
-            x /= 2;
+            primes[mul] = -1;
+            c++;
+            mul = p * c;
         }
-        int mul = 1;
-        for (int k = 0; k < 4; k++)
-            mul *= pow(a[k], bin[k]);
-        cout << pow(g, mul)<<" ";
     }
+}
+ 
+void print_all_primes()
+{
+    int c = 0;
+    for (int i = 0; i < MAX_NUM; i++)
+    {
+        if (primes[i] == 1)
+        {
+            c++;
+ 
+            if (c < 4)
+            {
+                switch (c)
+                {
+                    case 1:
+                        cout << c << "st prime is: " << i << endl;
+                        break;
+                    case 2:
+                        cout << c << "nd prime is: " << i << endl;
+                        break;
+                    case 3:
+                        cout << c << "rd prime is: " << i << endl;
+                        break;
+ 
+                    default:
+                        break;
+                }
+            }
+ 
+            else
+                cout << c << "th prime is: " << i << endl;
+        }
+    }
+}
+ 
+int main()
+{
+    gen_sieve_primes();
+    print_all_primes();
+    return 0;
 }
